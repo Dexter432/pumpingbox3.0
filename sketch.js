9d5fe74e5a4bc88c0c6d478f589d7ad8936ea1d2
Dexter432 <95933253+Dexter432@users.noreply.github.com>
2022-06-15 14:15:36 -0300

    pumpingbox4.0

--- sketch.js
+++ sketch.js
@@ -1,59 +1,84 @@
-var canvas;
-var block1,block2,block3,block4;
-var ball, edges;
+
+var block1, block2, block3, block4;
+var ball;
 var music;
 
 function preload(){
-    music = loadSound("music.mp3");
+music=loadSound("music.mp3");
 }
 
-
 function setup(){
-    canvas = createCanvas(800,600);
+    createCanvas(800,600);
 
-    block1 = createSprite(0,580,360,30);
-    block1.shapeColor = rgb(0,0,255);
+    
+    ball=createSprite(random(10,750),300,20,20);
+    ball.shapeColor="white";
+    ball.velocityX=3;
+    ball.velocityY=3;
+    
+   
+    block1=createSprite(100,580,180,30);
+    block1.shapeColor="red";
 
-    block2 = createSprite(295,580,200,30);
-    block2.shapeColor = rgb(255,128,0);
+    block2=createSprite(300,580,180,30);
+    block2.shapeColor="green";
 
-    block3 = createSprite(515,580,200,30);
-    block3.shapeColor = rgb(153,0,76);
+    block3=createSprite(500,580,180,30);
+    block3.shapeColor="blue";
 
-    block4 = createSprite(740,580,220,30);
-    block4.shapeColor = rgb(0,100,0);
+    block4=createSprite(700,580,180,30);
+    block4.shapeColor="yellow";
 
-    ball = createSprite(random(20,750),100, 40,40);
-    ball.shapeColor = rgb(255,255,255);
-    ball.velocityX = 4;
-    ball.velocityY = 9;
+    bordas=createEdgeSprites()
+    ball.bounceOff(bordas);
 
 }
 
 function draw() {
-    background(rgb(169,169,169));
-    edges=createEdgeSprites();
-    ball.bounceOff(edges);
-
-    if(block1.isTouching(ball) && ball.bounceOff(block1)){
-        ball.shapeColor = rgb(0,0,255);
-        music.play();
+    background(rgb(10,10,10))
+   
+    if( ball.x<0){
+      music.stop()
+      ball.velocityX=4
+    }else if( ball.x>800){
+      music.stop()
+      ball.velocityX=-3
     }
+   
+  if( ball.isTouching(block4)){
+    music.play()
+    ball.shapeColor="yellow";
+    ball.bounceOff(block4);
+    
+  }
+  
+  else if( ball.isTouching(block3)){
+    music.stop()
+    ball.shapeColor="blue";
+    bounceOff( ball,block3)
+   
+  }
+    
+  else if( ball.isTouching(block2)){
+    music.stop()
+    ball.shapeColor="green";
+    bounceOff( ball,block2)
+    ball.velocityX=3;
+    ball.velocityY=3;
+  }
+  
+  else if( ball.isTouching(block1)){
+    music.stop()
+    ball.shapeColor="red";
+    ball.bounceOff(block1)
+  }
 
-    if(block2.isTouching(ball)){
-        ball.shapeColor = rgb(255,128,0);
-        ball.velocityX = 0;
-        ball.velocityY = 0;
-        music.stop();
-    }
+  if ( ball.y<0){
+    music.stop()
+    ball.velocityY=3
+  }
 
-    if(block3.isTouching(ball) && ball.bounceOff(block3)){
-        ball.shapeColor = rgb(153,0,76);
-    }
-
-    if(block4.isTouching(ball) && ball.bounceOff(block4)){
-        ball.shapeColor = rgb(0,100,0);
-    }
-
-    drawSprites();
+  
+   
+    drawSprites()
 }
